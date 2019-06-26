fc73fbb05abd7dca18ecd54987946b3fb7149bbf
Bob den Os <bobdenos@gmail.com>
2019-06-26 19:31:03 +0200

    Refactor materials list to be a sub list

--- smelter/materials.lua
+++ smelter/materials.lua
@@ -1,187 +1,231 @@
 local NAME = ...
 
-local M = {}
+local table = require("table")
+
+local materials = {}
 
 -- Native metals
 
-M.iron = {solid = {"Iron Ingot"}}
+materials.iron = {solid = {"Iron Ingot"}}
 
-M.nickel = {solid = {"Nickel Ingot"}, liquid = {"Molten Nickel"}}
+materials.nickel = {solid = {"Nickel Ingot"}, liquid = {"Molten Nickel"}}
 
-M.copper = {solid = {"Copper Ingot"}, liquid = {"Molten Copper"}}
+materials.copper = {solid = {"Copper Ingot"}, liquid = {"Molten Copper"}}
 
-M.aluminum = {solid = {"Alumin Ingot"}, liquid = {"Molten Aluminum"}}
+materials.aluminum = {solid = {"Alumin Ingot"}, liquid = {"Molten Aluminum"}}
 
-M.osmium = {solid = {"Osmium Ingot"}, liquid = {"Molten Osmium"}}
+materials.osmium = {solid = {"Osmium Ingot"}, liquid = {"Molten Osmium"}}
 
-M.silver = {solid = {"Silver Ingot"}, liquid = {"Molten Silver"}}
+materials.silver = {solid = {"Silver Ingot"}, liquid = {"Molten Silver"}}
 
-M.cobalt = {solid = {"Cobalt Ingot"}, liquid = {"Molten Cobalt"}}
+materials.cobalt = {solid = {"Cobalt Ingot"}, liquid = {"Molten Cobalt"}}
 
-M.ardite = {solid = {"Ardite Ingot"}, liquid = {"Molten Ardite"}}
+materials.ardite = {solid = {"Ardite Ingot"}, liquid = {"Molten Ardite"}}
 
-M.platinum = {solid = {"Platinum Ingot"}, liquid = {"Molten Platinum"}}
+materials.platinum = {solid = {"Platinum Ingot"}, liquid = {"Molten Platinum"}}
 
-M.lead = {solid = {"Lead Ingot"}, liquid = {"Molten Lead"}}
+materials.lead = {solid = {"Lead Ingot"}, liquid = {"Molten Lead"}}
 
-M.boron = {solid = {"Boron Ingot"}, liquid = {"Molten Boron"}}
+materials.boron = {solid = {"Boron Ingot"}, liquid = {"Molten Boron"}}
 
-M.tin = {solid = {"Tin Ingot"}, liquid = {"Molten Tin"}}
+materials.tin = {solid = {"Tin Ingot"}, liquid = {"Molten Tin"}}
 
-M.gold = {solid = {"Gold Ingot", "minecraft:gold_ingot"}}
+materials.gold = {solid = {"Gold Ingot", "minecraft:gold_ingot"}}
 
-M.uranium = {solid = {"Uranium Ingot"}, liquid = {"Molten Uranium"}}
+materials.uranium = {solid = {"Uranium Ingot"}, liquid = {"Molten Uranium"}}
 
-M.manganese = {solid = {"Manganese Ingot"}, liquid = {"Molten Manganese"}}
+materials.manganese = {
+    solid = {"Manganese Ingot"},
+    liquid = {"Molten Manganese"}
+}
 
-M.manganeseDioxide = {
+materials.manganeseDioxide = {
     solid = {"Manganese Dioxide Ingot"},
     liquid = {"Molten Manganese Dioxide"}
 }
 
 -- Special metals
 
-M.refinedGlowstone = {
+materials.refinedGlowstone = {
     solid = {"Glowstone Ingot"},
     liquid = {"Refined Glowstone"}
 }
 
-M.refinedObsidian = {
+materials.refinedObsidian = {
     solid = {"Refined Obsidian Ingot"},
     liquid = {"Molten Refined Obsidian"}
 }
 
-M.elementium = {solid = {"Elementium Ingot"}, liquid = {"Molten Elementium"}}
+materials.elementium = {
+    solid = {"Elementium Ingot"},
+    liquid = {"Molten Elementium"}
+}
 
-M.manasteel = {solid = {"Manasteel Ingot"}, liquid = {"Molten Manasteel"}}
+materials.manasteel = {
+    solid = {"Manasteel Ingot"},
+    liquid = {"Molten Manasteel"}
+}
 
-M.terrasteel = {solid = {"Terrasteel Ingot"}, liquid = {"Molten Terrasteel"}}
+materials.terrasteel = {
+    solid = {"Terrasteel Ingot"},
+    liquid = {"Molten Terrasteel"}
+}
 
-M.lithium = {solid = {"Lithium Ingot"}, liquid = {"Molten Lithium"}}
+materials.lithium = {solid = {"Lithium Ingot"}, liquid = {"Molten Lithium"}}
 
-M.electricalSteel = {
+materials.electricalSteel = {
     liquid = {"Molten Electrical Steel"},
     solid = {"Electrical Steel Ingot"}
 }
 
-M.manaInfused = {solid = {"Mana Infused Ingot"}, liquid = {"Molten Mithril"}}
+materials.manaInfused = {
+    solid = {"Mana Infused Ingot"},
+    liquid = {"Molten Mithril"}
+}
 
-M.magnesium = {solid = {"Magnesium Ingot"}, liquid = {"Molten Magnesium"}}
+materials.magnesium = {
+    solid = {"Magnesium Ingot"},
+    liquid = {"Molten Magnesium"}
+}
 
-M.redstoneAlloy = {
+materials.redstoneAlloy = {
     solid = {"Redstone Alloy Ingot"},
     liquid = {"Molten Redstone Alloy"}
 }
 
-M.starmetal = {
+materials.starmetal = {
     solid = {"Starmetal Ingot"},
     liquid = {"Molten Astral Starmetal"}
 }
 
-M.draconium = {solid = {"Draconium Ingot"}, liquid = {"Molten Draconium"}}
+materials.draconium = {
+    solid = {"Draconium Ingot"},
+    liquid = {"Molten Draconium"}
+}
 
-M.soularium = {solid = {"Soularium Ingot"}, liquid = {"Molten Soularium"}}
+materials.soularium = {
+    solid = {"Soularium Ingot"},
+    liquid = {"Molten Soularium"}
+}
 
-M.endSteel = {solid = {"End Steel Ingot"}, liquid = {"Molten End Steel"}}
+materials.endSteel = {
+    solid = {"End Steel Ingot"},
+    liquid = {"Molten End Steel"}
+}
 
-M.ironAlloy = {solid = {"Iron Alloy Ingot"}, liquid = {"Molten Iron Alloy"}}
+materials.ironAlloy = {
+    solid = {"Iron Alloy Ingot"},
+    liquid = {"Molten Iron Alloy"}
+}
 
-M.demon = {solid = {"Demon Ingot"}, liquid = {"Molten Demon Metal"}}
+materials.demon = {solid = {"Demon Ingot"}, liquid = {"Molten Demon Metal"}}
 
-M.enchanted = {solid = {"Enchanted Ingot"}, liquid = {"Enchanted Metal"}}
+materials.enchanted = {
+    solid = {"Enchanted Ingot"},
+    liquid = {"Enchanted Metal"}
+}
 
-M.evilInfusedIron = {
+materials.evilInfusedIron = {
     solid = {"Evil Infused Iron Ingot"},
     liquid = {"Evil Infused Iron"}
 }
 
-M.plutonium = {solid = {"Blutonium Ingot"}, liquid = {"Molten Plutonium"}}
+materials.plutonium = {
+    solid = {"Blutonium Ingot"},
+    liquid = {"Molten Plutonium"}
+}
 
-M.thorium = {solid = {"Thorium Ingot"}, liquid = {"Molten Thorium"}}
+materials.thorium = {solid = {"Thorium Ingot"}, liquid = {"Molten Thorium"}}
 
-M.beryllium = {solid = {"Beryllium Ingot"}, liquid = {"Molten Beryllium"}}
+materials.beryllium = {
+    solid = {"Beryllium Ingot"},
+    liquid = {"Molten Beryllium"}
+}
 
-M.psimetal = {solid = {"Psimetal Ingot"}, liquid = {"Molten Psimetal"}}
+materials.psimetal = {solid = {"Psimetal Ingot"}, liquid = {"Molten Psimetal"}}
 
-M.thaumium = {solid = {"Thaumium Ingot"}, liquid = {"Molten Thaumium"}}
+materials.thaumium = {solid = {"Thaumium Ingot"}, liquid = {"Molten Thaumium"}}
 
-M.fiery = {solid = {"Fiery Ingot"}, liquid = {"Molten Fiery"}}
+materials.fiery = {solid = {"Fiery Ingot"}, liquid = {"Molten Fiery"}}
 
-M.knightmetal = {solid = {"Knightmetal Ingot"}, liquid = {"Molten Knightmetal"}}
+materials.knightmetal = {
+    solid = {"Knightmetal Ingot"},
+    liquid = {"Molten Knightmetal"}
+}
 
-M.iridium = {solid = {"Iridium Ingot"}, liquid = {"Molten Iridium"}}
+materials.iridium = {solid = {"Iridium Ingot"}, liquid = {"Molten Iridium"}}
 
-M.titanium = {solid = {"Titanium Ingot"}, liquid = {"Molten Titanium"}}
+materials.titanium = {solid = {"Titanium Ingot"}, liquid = {"Molten Titanium"}}
 
 -- Mixed metals
 
-M.invar = {
+materials.invar = {
     solid = {"Invar Ingot"},
     liquid = {"Molten Invar"},
     mix = {iron = 2, nickel = 1, invar = 3}
 }
 
-M.steel = {
+materials.steel = {
     solid = {"Steel Ingot"},
     liquid = {"Molten Steel"},
     mix = {iron = 9, coal = 25, steel = 9}
 }
 
-M.alumite = {
+materials.alumite = {
     solid = {"Alumite Ignot"},
     liquid = {"Molten Alumite"},
     mix = {aluminum = 5, iron = 2, obsidian = 2, alumite = 3}
 }
 
-M.knightslime = {
+materials.knightslime = {
     solid = {"Knightslime Ingot"},
     liquid = {"Molten Knightslime"},
     mix = {iron = 72, purpleSlime = 125, searedStone = 144, knightslime = 72}
 }
 
-M.pigiron = {
+materials.pigiron = {
     solid = {"Pigiron Ingot"},
     liquid = {"Molten Pigiron"},
     mix = {iron = 144, blood = 40, clay = 72, pigiron = 144}
 }
 
-M.conductiveIron = {
+materials.conductiveIron = {
     solid = {"Conductive Iron Ingot"},
     liquid = {"Molten Conductive Iron"},
     mix = {redstone = 25, iron = 36, conductiveIron = 36}
 }
 
-M.pulsatingIron = {
+materials.pulsatingIron = {
     solid = {"Pulsating Iron Ingot"},
     liquid = {"Molten Pulsating Iron"},
     mix = {iron = 72, ender = 125, pulsatingIron = 72}
 }
 
-M.darkSteel = {
+materials.darkSteel = {
     solid = {"Dark Steel Ingot"},
     liquid = {"Molten Dark Steel"},
     mix = {iron = 36, coal = 25, obsidian = 72, darkSteel = 36}
 }
 
-M.constantan = {
+materials.constantan = {
     solid = {"Constantan Ingot"},
     liquid = {"Molten Constantan"},
     mix = {nickel = 1, copper = 1, constantan = 2}
 }
 
-M.osgloglas = {
+materials.osgloglas = {
     solid = {"Osgloglas Ingot"},
     liquid = {"Molten Osgloglas"},
     mix = {refinedGlowstone = 1, refinedObsidian = 1, osmium = 1, osgloglas = 1}
 }
 
-M.osmiridium = {
+materials.osmiridium = {
     solid = {"Osmiridium Ingot"},
     liquid = {"Molten Osmiridium"},
     mix = {osmium = 1, iridium = 1, osmiridium = 2}
 }
 
-M.mirion = {
+materials.mirion = {
     solid = {"Mirion Ingot"},
     liquid = {"Molten Mirion"},
     mix = {
@@ -194,75 +238,78 @@ M.mirion = {
     }
 }
 
-M.signalium = {
+materials.signalium = {
     solid = {"Signalum Ingot"},
     liquid = {"Molten Signalum"},
     mix = {signalium = 144, redstone = 250, silver = 36, copper = 108}
 }
 
-M.enderium = {
+materials.enderium = {
     solid = {"Enderium Ingot"},
     liquid = {"Molten Enderium"},
     mix = {enderium = 144, ender = 250, platinum = 36, lead = 108}
 }
 
-M.ferroboron = {
+materials.ferroboron = {
     solid = {"Ferroboron Alloy"},
     liquid = {"Molten Ferroboron"},
     mix = {ferroboron = 2, boron = 1, steel = 1}
 }
 
-M.toughAlloy = {
+materials.toughAlloy = {
     solid = {"Tough Alloy"},
     liquid = {"Molten Tough Alloy"},
     mix = {touchAlloy = 2, lithium = 1, ferroboron = 1}
 }
 
-M.hardCarbon = {
+materials.hardCarbon = {
     solid = {"Hard Carbon Alloy"},
     liquid = {"Molten Hard Carbon"},
     mix = {hardCarbon = 144, diamond = 333, coal = 100}
 }
 
-M.manyullyn = {
+materials.manyullyn = {
     solid = {"Manyullyn Ingot"},
     liquid = {"Molten Manyullyn"},
     mix = {manyullyn = 1, ardite = 1, cobalt = 1}
 }
 
-M.bronze = {
+materials.bronze = {
     solid = {"Bronze Ingot"},
     liquid = {"Molten Bronze"},
     mix = {bronze = 4, tin = 1, copper = 3}
 }
 
-M.electrum = {
+materials.electrum = {
     solid = {"Electrum Ingot"},
     liquid = {"Molten Electrum"},
     mix = {electrum = 2, silver = 1, gold = 1}
 }
 
-M.brass = {solid = {"Alchemical Brass Ingot"}, liquid = {"Molten Brass"}}
+materials.brass = {
+    solid = {"Alchemical Brass Ingot"},
+    liquid = {"Molten Brass"}
+}
 
-M.aluminumBrass = {
+materials.aluminumBrass = {
     solid = {"Aluminum Brass Ingot"},
     liquid = {"Molten Aluminum Brass"},
     mix = {aluminumBrass = 4, aluminum = 3, copper = 1}
 }
 
-M.energeticAlloy = {
+materials.energeticAlloy = {
     solid = {"Energetic Alloy Ingot"},
     liquid = {"Molten Energetic Alloy"},
     mix = {energeticAlloy = 72, glowstone = 125, gold = 72, redstone = 50}
 }
 
-M.vibrantAlloy = {
+materials.vibrantAlloy = {
     solid = {"Vibrant Alloy Ingot"},
     liquid = {"Molten Vibrant Alloy"},
     mix = {vibrantAlloy = 72, ender = 125, energeticAlloy = 72}
 }
 
-M.lumium = {
+materials.lumium = {
     solid = {"Lumium Ingot"},
     liquid = {"Molten Lumium"},
     mix = {lumium = 72, glowstone = 125, silver = 18, rin = 54}
@@ -270,37 +317,72 @@ M.lumium = {
 
 -- Non metals
 
-M.searedStone = {solid = {"Cobblestone"}, liquid = {"Seared Stone"}}
+materials.searedStone = {solid = {"Cobblestone"}, liquid = {"Seared Stone"}}
 
-M.coal = {solid = {"Coal", "Graphite Ingot"}, liquid = {"Liquifacted Coal"}}
+materials.coal = {
+    solid = {"Coal", "Graphite Ingot"},
+    liquid = {"Liquifacted Coal"}
+}
 
-M.obsidian = {
+materials.obsidian = {
     solid = {"Obsidian"},
     liquid = {"Molten Obsidian"},
     mix = {water = 125, lava = 125, obsidian = 36}
 }
 
-M.ender = {solid = {"Ender Pearl"}, liquid = {"Resonant Ender"}}
+materials.ender = {solid = {"Ender Pearl"}, liquid = {"Resonant Ender"}}
 
-M.redstone = {solid = {"Redstone"}, liquid = {"Destablilized Redstone"}}
+materials.redstone = {solid = {"Redstone"}, liquid = {"Destablilized Redstone"}}
 
-M.clay = {
+materials.clay = {
     solid = {"Clay"},
     liquid = {"Molten Clay"},
     mix = {clay = 144, dirt = 144, searedStone = 72, water = 250}
 }
 
-M.blood = {solid = {"Rotten Flesh"}, liquid = {"Blood"}}
+materials.blood = {solid = {"Rotten Flesh"}, liquid = {"Blood"}}
 
-M.purpleSlime = {solid = {"Slime Ball"}, liquid = {"Liquid Purple Slime"}}
+materials.purpleSlime = {
+    solid = {"Slime Ball"},
+    liquid = {"Liquid Purple Slime"}
+}
 
-M.pinkSlime = {
+materials.pinkSlime = {
     solid = {"Pink Slime Ingot"},
     liquid = {"Molten Reinforced Pink Slime"}
 }
 
-M.glass = {solid = {"Sand", "Glass"}, liquid = {"Liquid Glass"}}
+materials.glass = {solid = {"Sand", "Glass"}, liquid = {"Liquid Glass"}}
 
-M.diamond = {solid = {"Diamond"}, liquid = {"Molten Diamond"}}
+materials.diamond = {solid = {"Diamond"}, liquid = {"Molten Diamond"}}
+
+local solid = {}
+local liquid = {}
+local mix = {}
+
+for matId, matVal in pairs(materials) do
+    -- prepare solid map
+    for i, v in pairs(matVal.solid) do solid[v] = matId end
+    -- prepare liquid map
+    for i, v in paris(matVal.liquid) do liquid[v] = matId end
+    -- prepare mix list
+    for i, v in pairs(matVal.mix) do
+        mix[v] = mix[v] or {}
+        for subI, subV in pairs(matVal.mix) do
+            if not subV == v or subV == matId then
+                table.insert(mix[v], subV)
+            end
+        end
+    end
+end
+
+local M = {}
+
+M.materials = materials
+M.solid = solid
+M.liquid = liquid
+M.mix = mix
+
+function M.solidToMaterial(mat) end
 
 return M
